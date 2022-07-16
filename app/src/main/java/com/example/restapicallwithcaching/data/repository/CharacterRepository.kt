253c0d7969c90947c85e15b9e4da56199cfe1bbb
ramzan_sheba <ramzan@sheba.xyz>
2022-07-16 14:20:33 +0600

    api call checks on main activity

--- app/src/main/java/com/example/restapicallwithcaching/data/repository/CharacterRepository.kt
+++ app/src/main/java/com/example/restapicallwithcaching/data/repository/CharacterRepository.kt
@@ -5,7 +5,6 @@ import com.example.restapicallwithcaching.utils.performGetOperation
 import javax.inject.Inject
 
 class CharacterRepository  {
-
     private val remoteDataSource = CharacterRemoteDataSource()
     fun getRepos(query: String, sort: String, limit: Int) = performGetOperation(
         networkCall = { remoteDataSource.getRepos(query, sort, limit) }
